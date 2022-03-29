48f2c6a6a09c0809d337320212d6c610c22f661f
Ingrid_Ramos <ingridrcs@gmail.com>
2022-03-29 16:53:15 -0300

    colocaçao das imagens dos icones que leva para as paginas de bebida, explorar e comida

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import drinkIcon from '../images/drinkIcon.svg';
+import exploreIcon from '../images/exploreIcon.svg';
 
 export default function Footer() {
   return (
     <footer data-testid="footer">
-      <Link to="/drinks" data-testid="drinks-bottom-btn" />
-      <Link to="/explore" data-testid="explore-bottom-btn" />
-      <Link to="/foods" data-testid="food-bottom-btn" />
+      <h1>Olá</h1>
+      <Link
+        to="/drinks"
+        data-testid="drinks-bottom-btn"
+      >
+        <img src={ drinkIcon } alt="Ícone de drinks" />
+      </Link>
+      <Link to="/explore" data-testid="explore-bottom-btn">
+        <img src={ exploreIcon } alt="Ícone de explorar" />
+      </Link>
+      <Link to="/foods" data-testid="food-bottom-btn">
+        <img src={ mealIcon } alt="Ícone de comida" />
+      </Link>
     </footer>
   );
 }
