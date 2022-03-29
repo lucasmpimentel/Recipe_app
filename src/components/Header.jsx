410cc764e66280ebedd9f9c5b746a08b4f0a32c1
Luciana Bischoff <lu_bischoff@hotmail.com>
2022-03-29 16:22:50 -0300

    req-10-ongoing create and render props

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,29 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import ProfileIcon from '../images/profileIcon.svg';
+import SearchIcon from '../images/searchIcon.svg';
 
-export default function Header() {
+export default function Header({ searchTopBtn }) {
   return (
     <header>
       <h1> teste header </h1>
-      <p data-testid="profile-top-btn">Profile</p>
-      <p data-testid="page-title">Page Title</p>
-      <p data-testid="search-top-btn">Search</p>
+      <img
+        src={ ProfileIcon }
+        alt="Imagem do Perfil"
+        data-testid="profile-top-btn"
+      />
+      { searchTopBtn && (
+        <img
+          src={ SearchIcon }
+          alt="Buscar"
+          data-testid="search-top-btn"
+        />)}
     </header>
   );
 }
+
+Header.propTypes = {
+  searchTopBtn: PropTypes.bool.isRequired,
+};
+
+// importação de svg: https://stackoverflow.com/questions/42296499/how-to-display-svg-icons-svg-files-in-ui-using-react-component
