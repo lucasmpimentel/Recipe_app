aeb954e2c6a98fb75d6f0a62ce93aea3dfb53903
Luciana Bischoff <lu_bischoff@hotmail.com>
2022-03-30 19:49:11 -0300

    req-82-finished and 84, 85, 86 finished

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
@@ -7,6 +8,37 @@ export default function Profile() {
     <>
       <Header />
       <p data-testid="page-title">Profile</p>
+      <p data-testid="profile-email">
+        Este é meu email
+      </p>
+      <Link to="/done-recipes">
+        <button
+          data-testid="profile-done-btn"
+          type="button"
+        >
+          {' '}
+          Done Recipes
+        </button>
+      </Link>
+      <Link to="/favorite-recipes">
+        <button
+          data-testid="profile-favorite-btn"
+          type="button"
+        >
+          {' '}
+          Favorite Recipes
+        </button>
+      </Link>
+      <Link to="/logout">
+        <button
+          data-testid="profile-logout-btn"
+          type="button"
+          onClick={ clearStorage }
+        >
+          {' '}
+          Logout
+        </button>
+      </Link>
       <Footer />
     </>
   );
