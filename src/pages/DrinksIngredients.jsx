7c88a83f49013aeeb05d79ed2d6a024e6ccc0015
Ingrid_Ramos <ingridrcs@gmail.com>
2022-03-29 22:52:27 -0300

    finished-req-21-colocao de footer no drinksIngredients

--- src/pages/DrinksIngredients.jsx
+++ src/pages/DrinksIngredients.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 export default function DrinksIngredients() {
   return (
     <>
       <Header />
       <p data-testid="page-title">Explore Ingredients</p>
+      <Footer />
     </>
   );
 }
