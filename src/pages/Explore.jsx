8119db3b75740673868b610a44f37fa3bb1fbb0c
Luciana Bischoff <lu_bischoff@hotmail.com>
2022-03-30 16:55:50 -0300

    req-69-finished

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 
 export default function Explore() {
@@ -6,20 +7,24 @@ export default function Explore() {
     <>
       <Header />
       <p data-testid="page-title" searchTopBtn>Explore</p>
-      <button
-        data-testid="explore-foods"
-        type="button"
-      >
-        {' '}
-        Explore Foods
-      </button>
-      <button
-        data-testid="explore-drinks"
-        type="button"
-      >
-        {' '}
-        Explore Drinks
-      </button>
+      <Link to="/explore/foods">
+        <button
+          data-testid="explore-foods"
+          type="button"
+        >
+          {' '}
+          Explore Foods
+        </button>
+      </Link>
+      <Link to="/explore/drinks">
+        <button
+          data-testid="explore-drinks"
+          type="button"
+        >
+          {' '}
+          Explore Drinks
+        </button>
+      </Link>
     </>
   );
 }
