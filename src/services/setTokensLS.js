e4ad93a9a18dcabec8333d768696b924023acacb
Lucas Pimentel <lucas.mirandapimentel@gmail.com>
2022-03-31 01:02:30 -0300

    onGoing Req 2 ao 8

---
+++ src/services/setTokensLS.js
@@ -0,0 +1,7 @@
+export function setMealsToken(value) {
+  localStorage.setItem(mealsToken, value);
+}
+
+export function setCocktailsToken(value) {
+  localStorage.setItem(cocktailsToken, value);
+}
