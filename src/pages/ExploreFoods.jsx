fe1b819355995b65e19229f486ebb6f2a66b9580
Ingrid_Ramos <ingridrcs@gmail.com>
2022-03-31 01:19:12 -0300

    finished req74-created handleClick function with fetchApi to get the id of meals and drinks

--- src/pages/ExploreFoods.jsx
+++ src/pages/ExploreFoods.jsx
@@ -1,20 +1,43 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 export default function ExploreFoods() {
+  const history = useHistory();
+  async function handleClick() {
+    try {
+      const url = 'https://www.themealdb.com/api/json/v1/1/random.php';
+      const response = await fetch(url);
+      const randomFood = await response.json();
+      const { idMeal } = randomFood.meals[0];
+      history.push(`/foods/${idMeal}`);
+    } catch (error) {
+      return error;
+    }
+  }
+
   return (
     <>
       <Header />
       <p data-testid="page-title">Explore Foods</p>
+
       <Link to="/explore/foods/ingredients">
         <button type="button" data-testid="explore-by-ingredient">By Ingredient</button>
       </Link>
+
       <Link to="/explore/foods/nationalities">
         <button type="button" data-testid="explore-by-nationality">By Nationality</button>
       </Link>
-      <button type="button" data-testid="explore-surprise">Surprise me!</button>
+
+      <button
+        type="button"
+        data-testid="explore-surprise"
+        onClick={ handleClick }
+      >
+        Surprise me!
+      </button>
+
       <Footer />
     </>
   );
