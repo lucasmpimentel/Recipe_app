3e93bac82df5d7cecb22141510f690369665e6d9
Ingrid_Ramos <ingridrcs@gmail.com>
2022-04-04 01:01:46 -0300

    ongoing-75 e 76- fetchAPI and show img

--- src/pages/FoodsIngredients.jsx
+++ src/pages/FoodsIngredients.jsx
@@ -1,12 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 export default function FoodsIngredients() {
+  const [allIngredients, setAllIngredients] = useState([]);
+  const fetchIngredients = async () => {
+    const response = await fetch('https://www.themealdb.com/api/json/v1/1/list.php?i=list');
+    const results = await response.json();
+    const ingredients = results.meals;
+    const MAX = 12;
+    const getIngredients = ingredients.filter((_ingredients, index) => index < MAX);
+    setAllIngredients(getIngredients);
+    return response.ok ? Promise.resolve(results) : Promise.reject(results);
+  };
+  useEffect(() => {
+    fetchIngredients();
+  }, []);
   return (
     <>
       <Header />
       <p data-testid="page-title">Explore Ingredients</p>
+      {allIngredients.map(({ idIngredient, strIngredient }, index) => (
+        <div key={ idIngredient } data-testid={ `${index}-ingredient-card` }>
+          <img
+            src={ `https://www.themealdb.com/images/ingredients/${strIngredient}-Small.png` }
+            alt="Imagem do ingrediente"
+            width="100px"
+            data-testid={ `${index}-card-img` }
+          />
+          <p data-testid={ `${index}-card-name` }>{ strIngredient }</p>
+        </div>
+      ))}
       <Footer />
     </>
   );
