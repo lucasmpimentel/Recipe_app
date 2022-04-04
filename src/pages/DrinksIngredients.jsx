867b982fab1eca389fac8e9f09ed2caaadda6063
Ingrid_Ramos <ingridrcs@gmail.com>
2022-04-04 15:31:34 -0300

    ongoing req 75 - data-testid of drink

--- src/pages/DrinksIngredients.jsx
+++ src/pages/DrinksIngredients.jsx
@@ -14,7 +14,6 @@ export default function DrinksIngredients() {
     const getDrinksIngredients = drinksIngredients
       .filter((_allIngredients, index) => index < MAX);
     setAllDrinks(getDrinksIngredients);
-    console.log(allDrinks);
     return response.ok ? Promise.resolve(results) : Promise.reject(results);
   };
   useEffect(() => {
@@ -24,6 +23,16 @@ export default function DrinksIngredients() {
     <>
       <Header />
       <p data-testid="page-title">Explore Ingredients</p>
+      {allDrinks.map(({ strIngredient1 }, index) => (
+        <div key={ index } data-testid={ `${index}-ingredient-card` }>
+          <img
+            src={ `www.thecocktaildb.com/images/ingredients/${strIngredient1}-Small.png` }
+            alt="Imagem do drink"
+            data-testid={ `${index}-card-img` }
+          />
+          <p data-testid={ `${index}-card-name` }>{ strIngredient1 }</p>
+        </div>
+      ))}
       <Footer />
     </>
   );
