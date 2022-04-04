9d563e8be16cd6159dbf2331ea2acc6c30a07ffe
Ingrid_Ramos <ingridrcs@gmail.com>
2022-04-04 17:18:04 -0300

    ongoing-req-77-handleClick in foodsIngredients pag

--- src/pages/FoodsIngredients.jsx
+++ src/pages/FoodsIngredients.jsx
@@ -1,9 +1,15 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
+import { useHistory } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
+import Context from '../context/Context';
 
 export default function FoodsIngredients() {
   const [allIngredients, setAllIngredients] = useState([]);
+  const { setMealsRetrieved } = useContext(Context);
+
+  const history = useHistory();
+
   const fetchIngredients = async () => {
     const response = await fetch('https://www.themealdb.com/api/json/v1/1/list.php?i=list');
     const results = await response.json();
@@ -13,9 +19,20 @@ export default function FoodsIngredients() {
     setAllIngredients(getIngredients);
     return response.ok ? Promise.resolve(results) : Promise.reject(results);
   };
+
+  const handleClick = (async (strIngredient) => {
+    const response = await
+    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${strIngredient}`);
+    const results = await response.json();
+    const foodsIngredients = results.meals;
+    setMealsRetrieved(foodsIngredients);
+    history.push('/foods');
+  });
+
   useEffect(() => {
     fetchIngredients();
   }, []);
+
   return (
     <>
       <Header />
@@ -26,8 +43,8 @@ export default function FoodsIngredients() {
           data-testid={ `${index}-ingredient-card` }
           role="button"
           tabIndex={ index }
-          onClick={ handleClick }
-          onKeyPress={ (e) => e.key === 'Enter' && toDrinkDetail(idDrink) }
+          onClick={ () => handleClick(strIngredient) }
+          onKeyPress={ (e) => e.key === 'Enter' }
         >
           <img
             src={ `https://www.themealdb.com/images/ingredients/${strIngredient}-Small.png` }
